28ca0f28d872edb61635edfbd9ae83375f47000c
victorvegak <vvega28@outlook.com>
2024-01-27 08:32:16 -0600

    home page was finished

--- scripts/visitCounter.js
+++ scripts/visitCounter.js
@@ -1,16 +1,13 @@
 
 const visitsDisplay = document.querySelector(".visits");
 
-let totalVisits = Number(window.localStorage.getItem("total-visits")) || 0;
+let totalVisits = Number(window.localStorage.getItem("totalVisit")) || 0;
 
-if (totalVisits !== 0) {
+if (totalVisits > 0) {
     visitsDisplay.textContent = `Visit Count: ${totalVisits}`;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
 }
-else {
-    visitsDisplay.textContent = `This is your first visit. Welcome!`;
-}
+
 totalVisits++;
-localStorage.setItem("total-visits", totalVisits);
-
-
-
+localStorage.setItem("totalVisit", totalVisits);
