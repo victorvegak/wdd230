5e160ffaa2cd3966f54076c9f71087260950e8e5
victorvegak <vvega28@outlook.com>
2024-01-27 20:58:17 -0600

    Home page finished

--- week03/scripts/lazyload.js
+++ week03/scripts/lazyload.js
@@ -1,3 +1,3 @@
 const lastModifiedDate = new Date(document.lastModified);
 const lastModifiedElement = document.getElementById("lastModified");
-lastModifiedElement.textContent = "Last modified: " + lastModifiedDate.toDateString();
+lastModifiedElement.textContent = "@Victor Vega - Last modified: " + lastModifiedDate.toDateString();
