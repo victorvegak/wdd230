b9b778044d58e676795958a1a726c817142ef3df
victorvegak <vvega28@outlook.com>
2024-02-03 23:35:37 -0600

    chamber and home page updated

--- scripts/form.js
+++ scripts/form.js
@@ -8,3 +8,26 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+
+//password verification
+const key1 = document.querySelector("#password");
+const key2 = document.querySelector("#confirm_password");
+
+const message = document.querySelector("#messageform");
+
+key2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (key1.value !== key2.value) {
+        message.textContent = "❗Passwords DO NOT MATCH!";
+        message.style.visibility = "show";
+        key2.style.backgroundColor = "#fff0f3";
+        key2.value = "";
+        key2.focus();
+    } else {
+        message.style.display = "none";
+        key2.style.backgroundColor = "rgba(0,0,0,0.2)";
+        key2.style.color = "#000";
+    }
+}
