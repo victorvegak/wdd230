1e66e2f199e69cccfd2c2ac36765937b4c4e568d
victorvegak <vvega28@outlook.com>
2024-02-08 22:50:55 -0600

    HomePage Updated

---
+++ scripts/links.js
@@ -0,0 +1,17 @@
+const baseURL = "https://victorvegak.github.io/wdd230/";
+const linksURL = "https://victorvegak.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data)
+    //console.log(data);//
+}
+
+getLinks();
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let card
+    });
+}
